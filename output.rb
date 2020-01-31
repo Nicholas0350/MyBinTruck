631031e7d8e9e752dceea548dc8a8ec5c67b17c7
nicholas <web@zainaweb.com>
2020-02-01 07:31:51 +1100

    added readme Robot, Table

---
+++ output.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'awesome_print'
+
+
+require_relative 'table.rb'
+
+square = Table.new
+ 
---
+++ table.rb
@@ -0,0 +1,25 @@
+class Table
+  attr_accessor :row
+  attr_accessor :column
+
+  def roam
+    
+  end
+  
+  def move
+    
+  end
+
+  def commands
+    PLACE == ''
+    MOVE == ''
+    LEFT == ''
+    RIGHT == ''
+    REPORT == ''
+    
+  end
+  
+  
+
+
+end
