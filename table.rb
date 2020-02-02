653b561dcdaf38a244b83e6cad771d84a433af2a
nicholas <web@zainaweb.com>
2020-02-02 13:46:39 +1100

    Add method to place a robot on the table

--- table.rb
+++ table.rb
@@ -2,12 +2,11 @@ class Table
   attr_accessor :row
   attr_accessor :column
 
-
   def initialize(row, column)
     @row = row
     @column = column
+    @robots = []
   end
-
 
   def commands
     PLACE == ''
@@ -16,8 +15,16 @@ class Table
     RIGHT == ''
     REPORT == ''
   end
-  
-  
 
+  def place robot, x, y
+    existing_robot = @robots.find { |_robot| _robot == robot }
+    
+    unless existing_robot
+      existing_robot = robot
+      @robots << robot      
+    end
 
+    existing_robot.x = x
+    existing_robot.y = y
+  end
 end
