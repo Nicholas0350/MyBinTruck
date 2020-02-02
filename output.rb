bb2683c8611f4a692192ac37c452ea6f52b10b43
nicholas <web@zainaweb.com>
2020-02-02 13:47:13 +1100

    Update driver to place 2 robots and display the output

--- output.rb
+++ output.rb
@@ -1,10 +1,25 @@
 require 'pry'
 require 'awesome_print'
 
-
 require_relative 'table.rb'
+require_relative 'robot.rb'
 
+table = Table.new(5, 5)
 
-square = Table.new(2, 3)
-binding.pry
- 
+ap table
+
+robot = Robot.new
+
+ap robot
+
+table.place(robot, 0, 0)
+table.place(robot, 2, 2)
+
+ap table
+
+table.place(Robot.new, 3, 3)
+ap table
+
+# table.move(robot, :NORTH)
+
+ap table
